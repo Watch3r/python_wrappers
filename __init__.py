a54f35a635f5b9106555d614cbc37ba2462b4457
Watch3r <pohyzka@myseneca.ca>
2021-10-27 17:20:41 -0400

    Added try statement.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
-from python_wrappers.file_wrapper import file_io, file_work, file_process, logger
-from python_wrappers.google_drive_wrapper import google_drive
-from python_wrappers.google_sheets_wrapper import google_sheet
-from python_wrappers.ip_wrapper import _ip
-from python_wrappers.selenium_wrapper import _selenium, acts
-from python_wrappers.telegram_wrapper import telegram
+try:
+    from python_wrappers.file_wrapper import file_io, file_work, file_process, logger
+    from python_wrappers.google_drive_wrapper import google_drive
+    from python_wrappers.google_sheets_wrapper import google_sheet
+    from python_wrappers.ip_wrapper import _ip
+    from python_wrappers.selenium_wrapper import _selenium, acts
+    from python_wrappers.telegram_wrapper import telegram
+except:
+    from file_wrapper import file_io, file_work, file_process, logger
+    from google_drive_wrapper import google_drive
+    from google_sheets_wrapper import google_sheet
+    from ip_wrapper import _ip
+    from selenium_wrapper import _selenium, acts
+    from telegram_wrapper import telegram
